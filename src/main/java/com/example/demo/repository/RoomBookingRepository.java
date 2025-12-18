c9b39fd969c8fb0215ff1a2d5e152e042ec225f7
bala-amypo <bala@amypo.in>
2025-12-18 09:04:31 +0000

    User commit

--- src/main/java/com/example/demo/repository/RoomBookingRepository.java
+++ src/main/java/com/example/demo/repository/RoomBookingRepository.java
@@ -1,7 +1,13 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.models.Guest;
 import com.example.demo.models.RoomBooking;
 
 public interface RoomBookingRepository extends JpaRepository<RoomBooking, Long> {
+
+    List<RoomBooking> findByGuest(Guest guest);
 }
