867a2fabf1a7bbbc7403a2755ad2cbe5147cd36b
bala-amypo <bala@amypo.in>
2025-12-21 16:55:38 +0000

    User commit

--- src/main/java/com/example/demo/models/DigitalKey.java
+++ src/main/java/com/example/demo/models/DigitalKey.java
@@ -11,6 +11,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
+
 @Entity
 public class DigitalKey {
 
