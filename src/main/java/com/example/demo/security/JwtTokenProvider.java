8cdf10d911f27df5cce12c2ae64d837d81fe5bfe
bala-amypo <bala@amypo.in>
2025-12-24 14:41:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -0,0 +1,24 @@
+package com.example.demo.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+
+    public String generateToken(Authentication authentication) {
+        return "dummy-jwt-token";
+    }
+
+    public boolean validateToken(String token) {
+        return true;
+    }
+
+    public String getEmailFromToken(String token) {
+        return "test@example.com";
+    }
+
+    public String getRoleFromToken(String token) {
+        return "ROLE_USER";
+    }
+}
