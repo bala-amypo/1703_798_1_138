504694feef5e76e0fb0cd09f4cd36f38af153f7a
bala-amypo <bala@amypo.in>
2025-12-24 15:08:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/AccessLogRepository.java
+++ src/main/java/com/example/demo/repository/AccessLogRepository.java
@@ -5,12 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.AccessLog;
-import com.example.demo.model.DigitalKey;
-import com.example.demo.model.Guest;
 
 public interface AccessLogRepository extends JpaRepository<AccessLog, Long> {
 
-    List<AccessLog> findByDigitalKey(DigitalKey digitalKey);
+    List<AccessLog> findByDigitalKeyId(Long keyId);
 
-    List<AccessLog> findByGuest(Guest guest);
+    List<AccessLog> findByGuestId(Long guestId);
 }
--- src/main/java/com/example/demo/repository/DigitalKeyRepository.java
+++ src/main/java/com/example/demo/repository/DigitalKeyRepository.java
@@ -6,14 +6,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.DigitalKey;
-import com.example.demo.model.Guest;
-import com.example.demo.model.RoomBooking;
 
 public interface DigitalKeyRepository extends JpaRepository<DigitalKey, Long> {
 
-    Optional<DigitalKey> findByBooking(RoomBooking booking);
+    Optional<DigitalKey> findByBookingIdAndActiveTrue(Long bookingId);
 
-    Optional<DigitalKey> findByBookingAndActiveTrue(RoomBooking booking);
-
-    List<DigitalKey> findByBooking_Guest(Guest guest);
+    List<DigitalKey> findByBookingGuestId(Long guestId);
 }
