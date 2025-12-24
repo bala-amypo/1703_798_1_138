23fc5c233d031635d487cc08f4dfae505a1a4495
bala-amypo <bala@amypo.in>
2025-12-24 15:14:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/KeyShareRequestRepository.java
+++ src/main/java/com/example/demo/repository/KeyShareRequestRepository.java
@@ -4,13 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.demo.model.Guest;
 import com.example.demo.model.KeyShareRequest;
 
 public interface KeyShareRequestRepository
         extends JpaRepository<KeyShareRequest, Long> {
 
-    List<KeyShareRequest> findBySharedBy(Guest sharedBy);
+    List<KeyShareRequest> findBySharedById(Long guestId);
 
-    List<KeyShareRequest> findBySharedWith(Guest sharedWith);
+    List<KeyShareRequest> findBySharedWithId(Long guestId);
 }
